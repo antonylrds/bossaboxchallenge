dd45cfd6a0a4f3dd9912b4e1cf2f7d5f80f8f3a1
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-01 20:35:59 -0300

    Crud compelto em controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,9 +9,60 @@ class UserController {
     if (user) {
       return res.status(400).json({ error: 'Email already exists' });
     }
-    const newUser = await User.create(req.body);
 
-    return res.json(newUser);
+    const { id, name } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async index(req, res) {
+    const users = await User.findAll();
+
+    return res.json(users);
+  }
+
+  async show(req, res) {
+    const user = await User.findByPk(req.params.id);
+    return res.json(user);
+  }
+
+  async update(req, res) {
+    const user = await User.findByPk(req.params.id);
+    const { oldPassword, email } = req.body;
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    console.log(id, name);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async delete(req, res) {
+    const user = await User.findByPk(req.params.id);
+
+    user.destroy();
+
+    return res.json({ msg: 'User deleted successfully' });
   }
 }
 
