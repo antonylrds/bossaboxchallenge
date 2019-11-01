0f572e73e63137a67fecc77b7a56f479471ac117
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-01 20:34:20 -0300

    crud de users e rota de login criados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', UserController.store);
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(AuthMiddleware);
+
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 export default routes;
