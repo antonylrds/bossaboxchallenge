552f0cd8d7e824438f708183e7858acd4bc5b9fb
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-01 07:55:32 -0300

    Database trocado para rodar com docker toolbox

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
-  dialect: 'pgsql',
-  host: 'localhost',
+  dialect: 'postgres',
+  host: '192.168.99.100',
   port: 5436,
+  database: 'bossabox',
   username: 'postgres',
   password: 'docker',
   define: {
