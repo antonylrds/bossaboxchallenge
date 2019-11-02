92ecc46f8d6bf890f0fca8fb266b6635bb60fcfd
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-02 00:49:11 -0300

    Rotas de ferramentas criadas

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import AuthMiddleware from './app/middlewares/auth';
+import ToolController from './app/controllers/ToolController';
 
 const routes = new Router();
 
@@ -16,5 +17,11 @@ routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
+
+routes.get('/tools', ToolController.index);
+routes.post('/tools', ToolController.store);
+routes.get('/tools/:id', ToolController.show);
+routes.put('/tools/:id', ToolController.update);
+routes.delete('/tools/:id', ToolController.delete);
 
 export default routes;
